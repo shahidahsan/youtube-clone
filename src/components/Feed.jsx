0d360c09721d5f4c54ff74ac9fdb63bb31508be5
Ahsan Shahid <ahsanshahidcool@gmail.com>
2022-11-21 13:52:49 -0500

    Youtube video search implement and video feeds UI completed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -2,14 +2,28 @@ import { useState, useEffect } from 'react';
 import { Box, Stack, Typography } from '@mui/material';
 
 import { Sidebar, Videos} from './';
+import { fetchFromAPI } from '../utils/fetchFromAPI';
 
 const Feed = () => {
+  const [selectedCategory, setSelectedCategory] = useState('New');
+  const [videos, setVideos]= useState([]);
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
+    .then((data) =>  {
+      setVideos(data.items)
+    });
+  }, [selectedCategory]);
+
   return (
     <Stack 
     sx={{ flexDirection: { sx: "column", md: "row"}}}
     >
       <Box sx={{ height: {sx: 'auto', md: '92vh'}, borderRight: '1px solid #3d3d3d', px: {sx: 0, md:2}  }}>
-        <Sidebar/>
+        <Sidebar
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+        />
         <Typography className="copyright" variant="body2"
         sx={{mt: 1.5, color: '#fff'}}>
           Copyright 2022 Ahsan Shahid Media
@@ -21,11 +35,10 @@ const Feed = () => {
         <Typography variant="h4"
         fontWeight="bold" mb={2} 
         sx={{ color: 'white'}}>
-          New <span style={{ color: '#F31503'}}>videos</span>
+          {selectedCategory} <span style={{ color: '#F31503'}}>videos</span>
         </Typography>
+        <Videos videos={videos}/>
       </Box>
-
-      <Videos videos={[]}/>
     </Stack>
   )
 }
